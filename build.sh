0fdbeb484e43bf25255c99eaa862173a6948254b
Jack Rose <user@jackrose.co.nz>
2023-05-14 23:38:17 +1000

    add code generated json schema for caddy.json
    not sure if it makes sense to use a json config, it's so much more
    verbose, but maybe that's useful in our use case?

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ binary_name="fecc"
 
 xcaddy build v${caddy_version} \
   --output out/${binary_name} \
+  --with github.com/abiosoft/caddy-json-schema \
   --with github.com/caddyserver/cache-handler@v${cache_handler_version}
+
+# save a json-schema file, including config for plugins
+./out/${binary_name} json-schema --vscode
