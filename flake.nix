b90cd497ff434b79f443ced3c7bd9cd42b0829b7
Jack Rose <user@jackrose.co.nz>
2023-05-14 21:36:15 +1000

    initial commit; direnv and flake setup

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "front end caddy caching reverse proxy (fecc-rp)";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ bashInteractive xcaddy go redis ];
+      };
+    });
+}
